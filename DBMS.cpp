a584932281f0dc10b39a6dd5ba10baf4ac3ef79e
Ross Hudgins <michael.ross.hudgins@gmail.com>
2013-09-05 01:28:32 -0500

    initial
    initial thoughts for a few functions and their parameters, need to
    discuss

---
+++ DBMS.cpp
@@ -0,0 +1,7 @@
+//=======================================
+// CSCE 315-501
+// Project 1 - Database Management System
+// Edgardo Angel
+// Lance Elliot
+// Ross Hudgins
+//=======================================
---
+++ DBMS.h
@@ -0,0 +1,43 @@
+//=======================================
+// CSCE 315-501
+// Project 1 - Database Management System
+// Edgardo Angel
+// Lance Elliot
+// Ross Hudgins
+//=======================================
+
+#include "stdlib.h"
+#include <string>
+#include <vector>
+
+class DBMS {
+	public:
+		vector< vector <string> > database;
+		
+			
+	//operations
+	//----------------------------------------------
+	//create table
+	//createTable(string tablename)
+	//
+	
+	//add entry
+	//addEntry(string type, vector of attributes?)
+	//length of row depends on type
+	
+	//remove entry
+	//removeEntry(primary key?)
+	
+	
+	//selection - select rows that satisfy a condition
+	//projection - select columns
+	//renaming attributes
+	//set union
+	//set difference
+	//cross product
+	
+	//write to file
+	//read from file
+	
+}
+
