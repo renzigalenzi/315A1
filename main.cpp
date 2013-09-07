04a44b56ef0ec5a6a3d1920237e49df412db1500
EdgardoA <edgardoangel@gmail.com>
2013-09-07 15:24:08 -0500

    testing

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+//=======================================
+// CSCE 315-501
+// Project 1 - Database Management System
+// Edgardo Angel
+// Lance Elliot
+// Ross Hudgins
+//=======================================
 
 #include "Table.h"
 
